e6181049d8d10fd86dd99a4348bab0514bd124be
rviprofile <rvi.profile@gmail.com>
2024-04-09 16:29:12 +0500

    env example

--- src/app.js
+++ src/app.js
@@ -4,10 +4,9 @@ const dotenv = require("dotenv");
 const userRouter = require("./routes/users.js");
 const bookRouter = require("./routes/books");
 const bodyParser = require("body-parser");
-// const loggerOne = require("./middlewares/loggerOne.js");
-// const loggerTwo = require("./middlewares/loggerTwo.js");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const logger = require("./middlewares/logger.js");
 
 dotenv.config();
 
@@ -27,46 +26,10 @@ mongoose
   });
 
 app.use(cors());
-// app.use(logger);
+app.use(logger);
 app.use(bodyParser.json());
 app.use(userRouter);
 app.use(bookRouter);
 app.listen(PORT, () =>
   console.log(`Сервер запущен по адресу ${API_URL}:${PORT}`)
 );
-
-// app.get("/", (request, response) => {
-//   response.status(200);
-//   response.send("Hello, World!");
-// });
-
-// app.use(loggerOne);
-// app.use(loggerTwo);
-
-// app.post("/", (request, response) => {
-//   response.status(200);
-//   response.send("Hello from POST!");
-// });
-
-// // app.get("/users/:id", (request, response) => {
-// //   const { id } = request.params;
-// //   response.status(200);
-// //   response.send(`User ID is ${id}`);
-// // });
-
-// app.use(userRouter);
-// app.use(booksRouter);
-
-// app.listen(PORT, () => {
-//   console.log(`Сервер запущен по адресу ${API_URL}:${PORT}`);
-// });
-
-// // // Or:
-// // try {
-// //   await mongoose.connect("mongodb://127.0.0.1:27017/test");
-// // } catch (error) {
-// //   handleError(error);
-// // }
-
-// app.use(cors);
-// app.use(bodyParser.json());
